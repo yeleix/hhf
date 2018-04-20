214cae9a6eb2c59c10dc7680376517cd2243611c
yeleix <yelei.xiao@gmail.com>
2018-04-20 03:38:25 -0400

    Perform more styling

--- app/routes/items.js
+++ app/routes/items.js
@@ -9,15 +9,39 @@ export default Route.extend({
       // console.log('hi')
       this.get('store').createRecord('item', newItem)
       .save()
+      .then(() => {
+        this.get('flashMessages')
+          .success('item was created!')
+      })
+      .catch(() => {
+        this.get('flashMessages')
+          .danger('There was a problem. Try again.')
+      })
     },
     deleteItem (item) {
       // console.log(item)
       item.destroyRecord()
         // .then(() => this.transitionTo('items'))
+        .then(() => {
+          this.get('flashMessages')
+            .success('item was deleted!')
+        })
+        .catch(() => {
+          this.get('flashMessages')
+            .danger('There was a problem. Try again.')
+        })
     },
     saveItem (item) {
-      console.log('item is ', item)
+      // console.log('item is ', item)
       item.save()
+      .then(() => {
+        this.get('flashMessages')
+          .success('item was updated!')
+      })
+      .catch(() => {
+        this.get('flashMessages')
+          .danger('There was a problem. Try again.')
+      })
     }
   }
 })
